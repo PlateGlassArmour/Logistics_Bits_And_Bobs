1ca6d5d18e92b0197f02eb5a687d530b4794caf8
PlateGlassArmour <49449154+PlateGlassArmour@users.noreply.github.com>
2019-04-11 17:09:09 -0400

    Update data.lua

--- data.lua
+++ data.lua
@@ -2,3 +2,4 @@ require("prototypes.item")
 require("prototypes.entity")
 require("prototypes.recipe")
 require("prototypes.technology")
+data.raw["roboport"]["roboport"]["fast_replaceable_group"] = "roboport"
